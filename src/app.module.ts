2948adead4e57292484fe083d5d8e40fb2131a30
Nils Morczinietz <nils.morczinietz@vodafonemail.de>
2025-12-31 16:58:33 +0100

    feat(deployment): add Docker configuration and deployment documentation

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,8 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { PostgresConnectionOptions } from 'typeorm/driver/postgres/PostgresConnectionOptions';
+import { dataSourceOptions } from './data-source';
 import { AlarmModule } from './modules/alarm/alarm.module';
 import { AuthModule } from './modules/auth/auth.module';
 import { DiveraModule } from './modules/divera/divera.module';
@@ -15,16 +17,18 @@ import { UserModule } from './modules/user/user.module';
     }),
     TypeOrmModule.forRootAsync({
       inject: [ConfigService],
-      useFactory: (configService: ConfigService) => ({
-        type: 'postgres',
-        host: configService.get('DB_HOST'),
-        port: Number(configService.get('DB_PORT')),
-        username: configService.get('DB_USERNAME'),
-        password: configService.get('DB_PASSWORD'),
-        database: configService.get('DB_DATABASE'),
-        entities: [__dirname + '/**/*.entity{.ts,.js}'],
-        synchronize: configService.get('NODE_ENV') !== 'production',
-      }),
+      useFactory: (configService: ConfigService): PostgresConnectionOptions =>
+        ({
+          ...dataSourceOptions,
+          type: 'postgres',
+          host: configService.get('DB_HOST'),
+          port: Number(configService.get('DB_PORT')),
+          username: configService.get('DB_USERNAME'),
+          password: configService.get('DB_PASSWORD'),
+          database: configService.get('DB_DATABASE'),
+          synchronize: configService.get('NODE_ENV') !== 'production',
+          migrationsRun: configService.get('NODE_ENV') === 'production',
+        }) as PostgresConnectionOptions,
     }),
     UserModule,
     AuthModule,
